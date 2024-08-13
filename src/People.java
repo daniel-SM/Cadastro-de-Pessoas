b64a6228bdbf76560d52d4ba079db86cb199079f
Daniel Santos <danieldsm.on@gmail.com>
2024-08-12 23:11:53 -0300

    add class People to represent real people

---
+++ src/People.java
@@ -0,0 +1,32 @@
+// Classe Pessoa para modelar uma pessoa real 
+public class People {
+  // Atributos de Pessoa
+  String name;
+  int age;
+  String phone;
+  String email;
+
+  // Construtor de Pessoa com todos os atributos
+  public People(String name, int age, String phone, String email) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  // Getter & Setter de "name"
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
+  
+  // Getter & Setter de "age"
+  public int getAge() { return age; }
+  public void setAge(int age) { this.age = age; }
+  
+  // Getter & Setter de "email"
+  public String getEmail() { return email; }
+  public void setEmail(String email) { this.email = email; }
+  
+  // Getter & Setter de "phone"
+  public String getPhone() { return phone; }
+  public void setPhone(String phone) { this.phone = phone; }  
+}
