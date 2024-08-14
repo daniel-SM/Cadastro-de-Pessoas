5d8ff4810feae739dbde7e7c77e824393b88ee11
Daniel Santos <danieldsm.on@gmail.com>
2024-08-14 12:10:59 -0300

    upgrade integration with storage manager and improve print styles

--- src/Actions.java
+++ src/Actions.java
@@ -9,11 +9,14 @@ public class Actions {
     this.storage = new Storage("data.txt");
   }
 
-  void create() {
+  public void create() {
     System.out.println("\n### Dados da Pessoa para criar");
 
     System.out.print("Digite o nome: ");
     String name = this.console.readLine();
+
+    System.out.print("Digite o idade: ");
+    int age = Integer.parseInt(this.console.readLine());
 
     System.out.print("Digite o telefone: ");
     String phone = this.console.readLine();
@@ -21,30 +24,39 @@ public class Actions {
     System.out.print("Digite o email: ");
     String email = this.console.readLine();
 
-    System.out.print("Digite o idade: ");
-    int age = Integer.parseInt(this.console.readLine());
-
     People people = new People(name, age, phone, email);
 
     // Chamando método para cadastrar nova pessoa
     this.storage.add(people);
 
-    System.out.println("\nCriado com sucesso!\n");
+    System.out.println("\n### Criado com sucesso!");
+    this.console.readLine("\nAperte enter para continuar...\n");
   }
 
-  void read() {
+  public void read() {
     System.out.println("\n### Dados da Pessoa para buscar");
 
     System.out.print("Digite o nome: ");
     String name = this.console.readLine();
 
     // Chamando método para buscar uma pessoa pelo nome
-    this.storage.search(name);
+    People p = this.storage.search(name);
 
-    System.out.println("\nEncontrado com sucesso!\n");
+    // Verificando se foi encontrado
+    if (p != null) {
+      System.out.println("\nPessoa encontrada com sucesso!");
+      System.out.println("Nome: " + p.getName());
+      System.out.println("Idade: " + p.getAge());
+      System.out.println("Telefone: " + p.getPhone());
+      System.out.println("Email: " + p.getEmail());
+
+    } else {
+      System.out.println("\nNão foi encontrada pessoa com este nome!");
+    }
+    this.console.readLine("\nAperte enter para continuar...\n");
   }
 
-  void update() {
+  public void update() {
     System.out.println("\n### Dados da Pessoa para atualizar");
 
     System.out.print("Digite o nome: ");
@@ -62,20 +74,32 @@ public class Actions {
     People people = new People(name, age, phone, email);
 
     // Chamando método para atualizar uma pessoa usando o nome
-    this.storage.update(people);
+    boolean success = this.storage.update(people);
 
-    System.out.println("\nAtualizado com sucesso!\n");
+    // Verificando se foi encontrado
+    if (success) {
+      System.out.println("\nAtualizado com sucesso!");
+    } else {
+      System.out.println("\nNão foi encontrada pessoa com este nome!");
+    }
+    this.console.readLine("\nAperte enter para continuar...\n");
   }
 
-  void delete() {
+  public void delete() {
     System.out.println("\n### Dados da Pessoa para apagar");
 
     System.out.print("Digite o nome: ");
     String name = this.console.readLine();
 
     // Chamando método para apagar uma pessoa usando o nome
-    this.storage.remove(name);
+    boolean success = this.storage.remove(name);
 
-    System.out.println("\nApagado com sucesso!\n");
+    // Verificando se foi encontrado
+    if (success) {
+      System.out.println("\nApagado com sucesso!");
+    } else {
+      System.out.println("\nNão foi encontrada pessoa com este nome!");
+    }
+    this.console.readLine("\nAperte enter para continuar...\n");
   }
 }
