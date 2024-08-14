22227353a546d0d5214d8be74f7c796fd8970fa2
Daniel Santos <danieldsm.on@gmail.com>
2024-08-14 12:06:49 -0300

    add new methods implementations to handle the people storage

--- src/Storage.java
+++ src/Storage.java
@@ -32,7 +32,7 @@ public class Storage {
         writer.newLine();
       }
     } catch (IOException e) {
-      System.out.println("Problema ao gravar arquivo!");
+      System.out.println("\nProblema ao gravar arquivo!");
       // TODO: handle exception
     }
   } // Fim da funcao write()
@@ -43,6 +43,7 @@ public class Storage {
 
     // Tentando ler arquivo
     try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+      // Percorrendo as linhas do arquivo
       String row;
       while ((row = reader.readLine()) != null) {
         // Separando a linha pela vírgula
@@ -60,12 +61,7 @@ public class Storage {
         list.add(p);
       }
     } catch (IOException e) { // Lidando com erro no arquivo
-      System.out.println("Problema ao ler arquivo!");
-      // TODO: handle exception
-
-    } catch (NumberFormatException e) { // Lidando com erro da conversao para inteiro
-      System.out.println("Formato de idade inválido!");
-      // TODO: handle exception
+      System.out.println("\nProblema ao ler arquivo!");
     }
 
     // Retornando a lista completa das pessoas do arquivo
@@ -73,19 +69,88 @@ public class Storage {
   } // Fim da funcao read()
 
   public void add(People p) {
-    ArrayList<People> list = new ArrayList<>();
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.path, true))) {
+      writer.write(p.toString());
+      writer.newLine();
+    } catch (IOException e) {
+      System.out.println("\nProblema ao gravar arquivo!");
+    }
+  } // Fim da funcao add()
 
-    list.add(p);
+  public People search(String name) {
+    // Variavel para guardar a pessoa encontrada
+    // ou nulo caso nao seja encontrada
+    People people = null;
 
-    write(list);
-  }
+    // Tentando ler arquivo
+    try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+      // Percorrendo as linhas do arquivo
+      String row;
+      while ((row = reader.readLine()) != null) {
+        // Separando a linha pela vírgula
+        String[] info = row.split(",");
 
-  public void search(String name) {
-  }
+        // Verificando se encontrou o nome informado
+        if (name.equals(info[0])) {
+          // Gerando novo objeto para adicionar na lista
+          people = new People(
+              info[0],                    // Nome
+              Integer.parseInt(info[1]),  // Idade
+              info[2],                    // Telefone
+              info[3]                     // Email
+          );
+        } // Fim do if
+      } // Fim do while
+    } catch (IOException e) { // Lidando com erro no arquivo
+      System.out.println("\nProblema ao ler arquivo!");
+    }
+    // Retornando a pessoa encontrada ou nulo caso nao seja encontrado
+    return people;
+  } // Fim da funcao search
 
-  public void update(People people) {
-  }
+  public boolean update(People people) {
+    // Lendo o arquivo e obtendo as pessoas
+    ArrayList<People> list = this.read();
 
-  public void remove(String name) {
-  }
+    // Percorrendo todos as pessoas
+    for (People p : list) {
+      // Verificando se encontrou a pessoa
+      if (people.getName().equals(p.getName())) {
+        // Atualizando os dados 
+        p.setAge(people.getAge());
+        p.setPhone(people.getPhone());
+        p.setEmail(people.getEmail());
+
+        // Salvando no arquivo
+        this.write(list);
+
+        // Retornando que foi bem-sucedido
+        return true;
+      }
+    }
+    // Retornando que nao encontrou pessoa 
+    return false;
+  } // Fim da funcao update()
+
+  public boolean remove(String name) {
+    // Lendo o arquivo e obtendo as pessoas
+    ArrayList<People> list = this.read();
+
+    // Percorrendo todos as pessoas
+    for (People p : list) {
+      // Verificando se encontrou a pessoa
+      if (name.equals(p.getName())) {
+        // Removendo a pessoa 
+        list.remove(p);
+
+        // Salvando no arquivo
+        this.write(list);
+
+        // Retornando que foi bem-sucedido
+        return true;
+      }
+    }
+    // Retornando que nao encontrou pessoa
+    return false;
+  } // Fim da funcao remove()
 }
